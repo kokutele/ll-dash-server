51449f9c028adab0d366bc2da4e9a39f840214d8
kensaku komatsu <kensaku.komatsu@gmail.com>
2021-11-28 20:06:14 +0900

    for issue #6 - added switch for disabling low latency mode

--- public/script.js
+++ public/script.js
@@ -1,29 +1,41 @@
 const videoEl = document.querySelector("#video-player")
 const startEl = document.querySelector("#btn-start")
+const lowLatencyEl = document.querySelector("#chk-lowlatency")
+
+let useLowLatency = true
 
 const start = () => {
   startEl.disabled = true
+  lowLatencyEl.disabled = true
 
   const url = '/dash/1.mpd'
 
   // eslint-disable-next-line no-undef
   const player = dashjs.MediaPlayer().create()
   player.initialize( videoEl, url, true )
-  player.updateSettings({
+  
+  const settings = useLowLatency ? {
     streaming: {
-         lowLatencyEnabled: true,
-          delay: {
-            liveDelay: 3
-         },
-         liveCatchup: {
-            minDrift: 0.02,
-            maxDrift: 0,
-            playbackRate: 0.5,
-            latencyThreshold: 60
-          }
+      lowLatencyEnabled: true,
+      delay: {
+        liveDelay: 3
+      },
+      liveCatchup: {
+        minDrift: 0.02,
+        maxDrift: 0,
+        playbackRate: 0.5,
+        latencyThreshold: 60
+      }
     }
-  });
+  }: {
+    streaming: {
+      lowLatencyEnabled: false,
+      lowLatencyEnabledByManifest: false
+    }
+  }
 
+  player.updateSettings( settings )
+   
   videoEl.addEventListener('loadedmetadata', async () => {
     await videoEl.play()
       .then( () => console.log('succeeded in playing video' ))
@@ -32,3 +44,6 @@ const start = () => {
 }
 
 startEl.addEventListener("click", start, false )
+lowLatencyEl.addEventListener("click", ev => {
+  useLowLatency = ev.target.checked
+})
